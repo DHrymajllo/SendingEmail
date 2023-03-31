36fdf294d30f9457059f924d17d5dd468925065c
Dorian Hrymajłło <44548837+DHrymajllo@users.noreply.github.com>
2023-03-31 17:37:20 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,67 @@
+﻿#pragma warning disable IDE0005
+using System;
+using System.Collections.Specialized;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using System.Threading;
+
+namespace ElasticEmailClient
+{
+    public class Program
+    {
+        static void Main(string[] args)
+        {
+            NameValueCollection values = new NameValueCollection();
+            values.Add("apikey", "B4681B288007926DAAD054033662A2C0A4AADCFB2A4D950667B6AD55CD7676F39AB950D251794626D53AAA20775F01E9");
+            values.Add("from", "d.hrymajllo@gmail.com");
+            values.Add("fromName", "Your Company Name");
+            //values.Add("to", "xyz@xyz.com"); // If u want send to one person
+            values.Add("subject", "Your Subject");
+            values.Add("bodyText", "Hello, {firstname}");
+            values.Add("bodyHtml", "<h1>Hello, {firstname} {lastname}</h1>");
+            values.Add("mergesourcefilename", "mycontacts.csv"); // From csv file
+
+            var filepath = "C:\\Users\\RyderX\\Desktop\\mycontacts.csv";
+            var file = File.OpenRead(filepath);
+
+            // multiple files can be sent using this method
+            var filesStream = new Stream[] { file };
+            var filenames = new string[] { "mycontacts.csv" };
+            var URL = "https://api.elasticemail.com/v2/email/send";
+
+            string result = Upload(URL, values, filesStream, filenames);
+
+            Console.WriteLine(result);
+        }
+
+        public static string Upload(string actionUrl, NameValueCollection values, Stream[] paramFileStream = null, string[] filenames = null)
+        {   
+            using (var client = new HttpClient())
+            using (var formData = new MultipartFormDataContent())
+            {
+                foreach (string key in values)
+                {
+                    HttpContent stringContent = new StringContent(values[key]);
+                    formData.Add(stringContent, key);
+                }
+
+                for (int i = 0; i < paramFileStream.Length; i++)
+                {
+                    HttpContent fileStreamContent = new StreamContent(paramFileStream[i]);
+                    formData.Add(fileStreamContent, "file" + i, filenames[i]);
+                }
+
+                var response = client.PostAsync(actionUrl, formData).Result;
+                if (!response.IsSuccessStatusCode)
+                {
+                    throw new Exception(response.Content.ReadAsStringAsync().Result);
+                }
+
+                return response.Content.ReadAsStringAsync().Result;
+            }
+        }
+
+    }
+}
---
+++ ProgramTests.cs
@@ -0,0 +1,33 @@
+﻿#pragma warning disable IDE0005
+using ElasticEmailClient;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Specialized;
+using System;
+
+namespace SendingEmail
+{
+    [TestClass]
+    public class ProgramTests
+    {
+        [TestMethod]
+        public void Upload_StateUnderTest_ExpectedBehavior()
+        {
+            // Arrange
+            var program = new Program();
+            string actionUrl = null;
+            NameValueCollection values = null;
+            Stream[] paramFileStream = null;
+            string[] filenames = null;
+
+            // Act
+            var result = Program.Upload(
+                actionUrl,
+                values,
+                paramFileStream,
+                filenames);
+
+            // Assert
+            Assert.Fail();
+        }
+    }
+}
